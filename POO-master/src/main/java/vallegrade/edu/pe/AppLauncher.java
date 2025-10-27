9c04a76ced7bf96c3d1eead7f87f247ffc2dcb9a
mariory-valeriano <mariory.valeriano@vallegrande.edu.pe>
2025-10-27 01:33:43 -0500

    Delete POO-master directory

--- POO-master/src/main/java/vallegrade/edu/pe/AppLauncher.java
+++
@@ -1,8 +0,0 @@
-package vallegrande.edu.pe.view;
-
-public class AppLauncher {
-    public static void main(String[] args) {
-        // Crear e iniciar el formulario de reclamos
-        new FrmClaim();
-    }
-}
